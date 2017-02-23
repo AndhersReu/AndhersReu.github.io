e7f86bcd1cfc3a292e606d602b9aa8dc2eaf17dd
AndhersReu <andhers_reu@hotmail.com>
2017-02-23 14:10:11 -0600

    Update FIGURA-EJEMPLO.js

--- FIGURA-EJEMPLO.js
+++ FIGURA-EJEMPLO.js
@@ -2,7 +2,7 @@
 
 var escena = new THREE.Scene();
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 2;
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerHeight*.9, window.innerHeight*.9 );
 document.body.appendChild( renderer.domElement );
@@ -11,8 +11,8 @@ renderer.render( escena, camara );
 
 var forma = new THREE.BoxGeometry( 1, 1, 1 );
 var cubo  = new THREE.Mesh( forma );
-cubo.rotation.x += 0.1;
-cubo.rotation.y += 0.1;
+cubo.rotation.x += 0.8;
+cubo.rotation.y += 0.8;
 escena.add( cubo );
 renderer.render( escena, camara );
 
