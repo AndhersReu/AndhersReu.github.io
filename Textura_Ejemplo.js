6680e9fe0da8f3d9da94716f37373bd0b96efb8c
AndhersReu <andhers_reu@hotmail.com>
2017-05-15 21:42:30 -0500

    Update Textura_Ejemplo.js

--- Textura_Ejemplo.js
+++ Textura_Ejemplo.js
@@ -1,6 +1,6 @@
 function setup() {
   THREE.ImageUtils.crossOrigin = '';
-  var textura   = THREE.ImageUtils.loadTexture('https://edsonvelazquez.github.io/brick_diffuse.jpg');
+  var textura   = THREE.ImageUtils.loadTexture('https://AndhersReu.github.io/ladrilloo.jpg');
   var material  = new THREE.MeshLambertMaterial( {map: textura} );
   var forma     = new THREE.BoxGeometry( 1, 4, 9 );
   malla         = new THREE.Mesh( forma, material );
